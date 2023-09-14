e7caa4484d0641ccc20906d463d249b806de0b2a
InTheBloom_on_Desktop <nato.rider.smm2@gmail.com>
2023-09-14 21:54:39 +0900

    fix

--- dlang/src/enumdivs.d
+++ dlang/src/enumdivs.d
@@ -8,7 +8,7 @@ struct enumDivs {
         auto msg = format("Line : %s, N must be greater than or equal to 0. Your input = %s.", __LINE__, N);
         enforce(0 <= N, msg);
 
-        div = new long[][](N, 0);
+        div = new long[][](N+1, 0);
         foreach (x; 1..N+1) {
             if (N < x*x) {
                 break;
@@ -17,23 +17,20 @@ struct enumDivs {
             long y = 1;
             while (x*y <= N) {
                 div[x*y] ~= x;
+                y++;
             }
-        }
-
-        foreach (ref d; div) {
-            d.sort;
         }
     }
 
     size_t length () {
-        div.length;
+        return div.length;
     }
 
     long[] opIndex (size_t i) {
         return div[i];
     }
 
-    long[] opSlice (size_t i, size_t j) {
+    long[][] opSlice (size_t i, size_t j) {
         return div[i..j];
     }
 
