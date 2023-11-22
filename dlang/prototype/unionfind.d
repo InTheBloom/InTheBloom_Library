97508ece099368b5b9914720c796f7565748e9cc
InTheBloom <nato.rider.smm2@gmail.com>
2023-11-22 14:13:13 +0900

    unionfindに対するいくつかの変更

--- dlang/prototype/unionfind.d
+++ dlang/prototype/unionfind.d
@@ -1,93 +1,16 @@
 /**
  * TODO
- *   - クラスの各関数の仕様をまとめる
- *   - コンストラクタ関数の仕様をまとめる
  *   - assertで落としたときにstderrにメッセージを表示
- *   - 関数名を変えるかもしれん(uniteとareInSameGroupとの整合性が取れてなさすぎ)
+ *   - リセット機能をつける
  *
  * VERYFYIED
- *   - uniteとareInSameGroup : yosupo judge (https://judge.yosupo.jp/problem/unionfind)
+ *   - uniteとsame : yosupo judge (https://judge.yosupo.jp/problem/unionfind)
  *
  * UNVERYFYIED
  *   - countGroups
  *   - GroupSize
- *   - enumerateGroups
+ *   - enumGroups
  */
-
-
-/* ------------------------------ */
-/*        class definition        */
-/* ------------------------------ */
-
-class UnionFind_Dictionary (T) {
-    private:
-        T[T] parent;
-        int[T] size;
-
-    T findRoot (T x) {
-        if (x !in parent) {
-            addElement(x);
-            return x;
-        }
-
-        if (parent[x] == x) return x;
-        return parent[x] = findRoot(parent[x]);
-    }
-
-    bool same (T x, T y) {
-        return findRoot(x) == findRoot(y);
-    }
-
-    void unite (T x, T y) {
-        addElement(x), addElement(y);
-        T larger, smaller;
-        if (GroupSize(x) <= GroupSize(y)) {
-            larger = findRoot(y);
-            smaller = findRoot(x);
-        } else {
-            larger = findRoot(x);
-            smaller = findRoot(y);
-        }
-
-        if (larger == smaller) return;
-
-        parent[smaller] = larger;
-        size[larger] += size[smaller];
-    }
-
-    int countGroups () {
-        int res = 0;
-        foreach (key, val; parent) {
-            if (findRoot(key) == key) res++;
-        }
-        return res;
-    }
-
-    bool addElement (T x) {
-        if (x in parent) return false;
-        parent[x] = x;
-        size[x] = 1;
-        return true;
-    }
-
-    int GroupSize (T x) {
-        addElement(x);
-        return size[findRoot(x)];
-    }
-
-    T[][] enumerateGroups (T x) {
-        T[][T] mp;
-        foreach (key, val; parent) {
-            mp[findRoot(key)] ~= key;
-        }
-
-        foreach (val; mp) {
-            res ~= val;
-        }
-
-        return res;
-    }
-}
 
 class UnionFind_Array {
     private:
@@ -109,13 +32,13 @@ class UnionFind_Array {
         }
     }
 
-    int findRoot (int x) 
+    int root (int x) 
     in {
         assert(0 <= x && x < N);
     }
     do {
         if (parent[x] == x) return x;
-        return parent[x] = findRoot(parent[x]);
+        return parent[x] = root(parent[x]);
     }
 
     bool same (int x, int y)
@@ -124,7 +47,7 @@ class UnionFind_Array {
         assert(0 <= y && y < N);
     }
     do {
-        return findRoot(x) == findRoot(y);
+        return root(x) == root(y);
     }
 
     void unite (int x, int y)
@@ -135,11 +58,11 @@ class UnionFind_Array {
     do {
         int larger, smaller;
         if (GroupSize(x) <= GroupSize(y)) {
-            larger = findRoot(y);
-            smaller = findRoot(x);
+            larger = root(y);
+            smaller = root(x);
         } else {
-            larger = findRoot(x);
-            smaller = findRoot(y);
+            larger = root(x);
+            smaller = root(y);
         }
 
         if (larger == smaller) return;
@@ -150,7 +73,7 @@ class UnionFind_Array {
 
     int countGroups () {
         int res = 0;
-        foreach (i; 0..N) if (findRoot(i) == i) res++;
+        foreach (i; 0..N) if (root(i) == i) res++;
         return res;
     }
 
@@ -159,20 +82,20 @@ class UnionFind_Array {
         assert(0 <= x && x < N);
     }
     do {
-        return size[findRoot(x)];
+        return size[root(x)];
     }
 
-    int[][] enumerateGroups (int x)
+    int[][] enumGroups (int x)
     in {
         assert(0 <= x && x < N);
     }
     do {
         int[][] mp = new int[][](N, 0);
-        foreach (i; 0..N)
-            mp[findRoot(i)] ~= i;
+        foreach (i; 0..N) {
+            mp[root(i)] ~= i;
         }
 
-        int[][] res = new int[][](resSize);
+        int[][] res;
         foreach (m; mp) {
             if (m.length == 0) continue;
             res ~= m;
@@ -180,28 +103,24 @@ class UnionFind_Array {
 
         return res;
     }
-}
 
-/* ------------------------------ */
-/*          Constructors          */
-/* ------------------------------ */
-
-/* Dictionary UF */
-auto UnionFind (T) () {
-    return new UnionFind_Dictionary!(T)();
-}
-
-import std.range.primitives : isInputRange;
-auto UnionFind (T, E) (E range) if (isInputRange!(E) || is(T == S[], S) || is(T == S[n], S, size_t n)) {
-    auto res = new UnionFind_Dictionary!(T)();
-    foreach (elem; range) {
-        res.addElement(elem);
+    void reset (int N = this.N)
+    in {
+        assert(0 <= N, "N must be positive integer.");
     }
+    do {
+        if (N != this.N) {
+            this.N = N;
+            parent.length = size.length = N;
+        }
 
-    return res;
+        foreach (i; 0..N) {
+            parent[i] = i;
+            size[i] = 1;
+        }
+    }
 }
 
-/* Array UF */
-auto UnionFind (T) (int N) if (is(T == int)) {
+auto UnionFind (int N) {
     return new UnionFind_Array(N);
 }
---
+++ dlang/prototype/unionfind_anytype.d
@@ -0,0 +1,88 @@
+class UnionFind_Dictionary (T) {
+    private:
+        T[T] parent;
+        int[T] size;
+
+    T root (T x) {
+        if (x !in parent) {
+            addElement(x);
+            return x;
+        }
+
+        if (parent[x] == x) return x;
+        return parent[x] = root(parent[x]);
+    }
+
+    bool same (T x, T y) {
+        return root(x) == root(y);
+    }
+
+    void unite (T x, T y) {
+        addElement(x), addElement(y);
+        T larger, smaller;
+        if (GroupSize(x) <= GroupSize(y)) {
+            larger = root(y);
+            smaller = root(x);
+        } else {
+            larger = root(x);
+            smaller = root(y);
+        }
+
+        if (larger == smaller) return;
+
+        parent[smaller] = larger;
+        size[larger] += size[smaller];
+    }
+
+    int countGroups () {
+        int res = 0;
+        foreach (key, val; parent) {
+            if (root(key) == key) res++;
+        }
+        return res;
+    }
+
+    bool addElement (T x) {
+        if (x in parent) return false;
+        parent[x] = x;
+        size[x] = 1;
+        return true;
+    }
+
+    int GroupSize (T x) {
+        addElement(x);
+        return size[root(x)];
+    }
+
+    T[][] enumGroups (T x) {
+        T[][T] mp;
+        foreach (key, val; parent) {
+            mp[root(key)] ~= key;
+        }
+
+        foreach (val; mp) {
+            res ~= val;
+        }
+
+        return res;
+    }
+
+    void reset () {
+        parent.clear;
+        size.clear;
+    }
+}
+
+auto UnionFind_Any (T) () {
+    return new UnionFind_Dictionary!(T)();
+}
+
+import std.range.primitives : isInputRange;
+auto UnionFind_Any (T, E) (E range) if (isInputRange!(E) || is(T == S[], S) || is(T == S[n], S, size_t n)) {
+    auto res = new UnionFind_Dictionary!(T)();
+    foreach (elem; range) {
+        res.addElement(elem);
+    }
+
+    return res;
+}
