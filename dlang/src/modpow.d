cb44b91dfacf5fa30420775a371edf5b5d2d46ff
InTheBloom_on_Desktop <nato.rider.smm2@gmail.com>
2023-12-13 23:11:06 +0900

    modpowの関数名変更

--- dlang/src/modpow.d
+++ dlang/src/modpow.d
@@ -1,4 +1,4 @@
-long modPow (long a, long x, const long MOD) {
+long ModPow (long a, long x, const long MOD) {
     import std.exception : enforce;
 
     enforce(0 <= x, "x must satisfy 0 <= x");
@@ -19,7 +19,7 @@ long modPow (long a, long x, const long MOD) {
     return res % MOD;
 }
 
-T modPow (T, X, Y) (T a, X x, const Y MOD) {
+T ModPow (T, X, Y) (T a, X x, const Y MOD) {
     import std.exception: enforce;
 
     enforce(0 <= x, "x must satisfy 0 <= x");
