f9762ffa7942e596f607b07385fb9178e5b1844b
InTheBloom_on_Desktop <nato.rider.smm2@gmail.com>
2023-12-17 06:19:30 +0900

    change function name of modinv

--- dlang/src/modinv.d
+++ dlang/src/modinv.d
@@ -1,7 +1,7 @@
-long modInv (const long x, const long MOD) {
+long ModInv (const long x, const long MOD) {
     import std.exception : enforce;
     import std.format : format;
     enforce(1 <= MOD, format("MOD must satisfy 1 <= MOD. Now MOD =  %s.", MOD));
     enforce(MOD <= int.max, format("MOD must satisfy MOD*MOD <= long.max. Now MOD = %s.", MOD));
-    return modPow(x, MOD-2, MOD);
+    return ModPow(x, MOD-2, MOD);
 }
