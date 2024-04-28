036ebf8a87b3b93b919b8b9b398e1a71f531d5a3
InTheBloom_on_Desktop <nato.rider.smm2@gmail.com>
2024-04-28 23:04:47 +0900

    primemodulofactorialをstatic class化した

--- dlang/prototype/prime_modulo_factorial.d
+++ dlang/prototype/prime_modulo_factorial.d
@@ -1,36 +1,54 @@
 // check mod_inv
 static assert(__traits(compiles, mod_inv(998244353, 1_000_000_007)));
 
-template PrimeModuloFactorial (ulong M)
-if ((1 <= M && M < int.max)
-    && ((x) {
-        for (int i = 2; i < x; i++) {
-            if (x < 1L*i*i) break;
-            if (x % i == 0) return false;
-        }
-        return true;
-    })(cast(int) M))
+class PrimeModuloFactorial (ulong M)
 {
+    /// methods:
+    /// - void build (ulong N_)
+    /// - long binom (ulong n_, ulong k_)
+    /// - long factorial (ulong x_)
+    /// - long factorial_inv (ulong x_)
+
     import std.conv : to;
     import std.format : format;
 
+    // varidate
+    static assert(1 <= M && M < int.max, format("PrimeModuloFactorial: M = %s is out of range. M must be in range of [1, %s].", M, int.max));
+
+    private static bool internal_is_prime (int x) {
+        if (x <= 1) return false;
+        foreach (i; 2..x + 1) {
+            if (x < 1L * i * i) break;
+            if (x % i == 0) return false;
+        }
+        return true;
+    }
+    static assert(internal_is_prime(M.to!int), format("PrimeModuloFactorial: M = %s is not prime number.", M));
+
     private:
-        long[] fact, fact_inv;
-        int N = 0;
-        long MOD = M;
+        static long[] fact, fact_inv;
+        static int N = 0;
+        static long MOD = M;
 
     public:
-        void build (ulong N_) {
+        @disable this () {}
+
+        static void build (ulong N_)
+        in {
+            assert(0 <= N_ && N_ <= 10^^8, format("N = %s does not satisfy constraints. N must be in range of [0, 10^8].", N_));
+        }
+        do {
+            if (N_ <= N) { N = N_.to!int; return; }
             N = N_.to!int;
-            fact.length = fact_inv.length = N+1;
+            fact.length = fact_inv.length = N + 1;
 
             fact[0] = 1;
-            for (int i = 1; i <= N; i++) fact[i] = i * fact[i-1] % MOD;
+            for (int i = 1; i <= N; i++) fact[i] = i * fact[i - 1] % MOD;
             fact_inv[N] = mod_inv(fact[N], MOD);
-            for (int i = N; 0 < i; i--) fact_inv[i-1] = i * fact_inv[i] % MOD;
+            for (int i = N; 0 < i; i--) fact_inv[i - 1] = i * fact_inv[i] % MOD;
         }
 
-        long binom (ulong n_, ulong k_)
+        static long binom (ulong n_, ulong k_)
         in {
             assert((n_ < k_
                     || (n_ <= N && k_ <= N)),
@@ -43,10 +61,10 @@ if ((1 <= M && M < int.max)
 
             if (n < k) return 0;
             long res = fact[n] * fact_inv[k] % MOD;
-            return res * fact_inv[n-k] % MOD;
+            return res * fact_inv[n - k] % MOD;
         }
 
-        long factorial (ulong x_)
+        static long factorial (ulong x_)
         in {
             assert(x_ <= N,
                     format("Out of range of pre-calculation. MAX = %s, x = %s.", N, x_),
@@ -57,7 +75,7 @@ if ((1 <= M && M < int.max)
             return fact[x];
         }
 
-        long factorial_inv (ulong x_)
+        static long factorial_inv (ulong x_)
         in {
             assert(x_ <= N,
                     format("Out of range of pre-calculation. MAX = %s, x = %s.", N, x_)
