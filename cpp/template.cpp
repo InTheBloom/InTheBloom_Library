58ff4a21431dcf432df4fae2b60bd1fec3bf2708
InTheBloom_on_Desktop <nato.rider.smm2@gmail.com>
2024-06-29 00:23:22 +0900

    add cpp template

---
+++ cpp/template.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+using ll = long long;
+
+constexpr int iINF = 1'000'000'000;
+constexpr ll llINF = 1'000'000'000'000'000'000;
+
+int main () {
+}
